9dd98a9b826139f487fdb648036b06a1134eb48d
Edmond Hui <edmond.t.hui@gmail.com>
2021-02-14 20:47:54 -0500

    add play button and sound effects

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const cards = document.querySelectorAll('.card');
-  const opponent = document.querySelector('.opponent');
+  const cards = document.querySelectorAll(".card");
+  const opponent = document.querySelector(".opponent");
+  const playButton = document.querySelector(".play");
   let selected;
 
+  playButton.addEventListener("click", play);
 
-  cards.forEach(card => {
-    if (!card.parentElement.classList.contains('opponent')) {
-      card.addEventListener('click', selectCard)
+  cards.forEach((card) => {
+    if (!card.parentElement.classList.contains("opponent")) {
+      card.addEventListener("click", selectCard);
     }
   });
 
   function selectCard(e) {
     selected = e.currentTarget;
-    let selectedElements = document.querySelectorAll('.selected')
+    let selectedElements = document.querySelectorAll(".selected");
     if (selectedElements.length < 1) {
-      e.currentTarget.classList.add('selected');
+      e.currentTarget.classList.add("selected");
     } else {
-      selectedElements[0].classList.remove('selected');
-      e.currentTarget.classList.add('selected');
+      selectedElements[0].classList.remove("selected");
+      e.currentTarget.classList.add("selected");
     }
   }
-})
+
+  function play(e) {
+    e.preventDefault();
+    cards.forEach((card) => {
+      if (card.classList.contains("selected")) {
+        card.classList.remove("selected");
+        let audio = new Audio("zawazawa.wav");
+        audio.play();
+      }
+    });
+  }
+});
