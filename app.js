5f71605e884f35d11fc271498ecb8241d4c74cf9
Edmond Hui <edmond.t.hui@gmail.com>
2021-02-14 23:08:52 -0500

    add styling and images to cards

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+  
+  // make a function that adds cards to each players hand based on whether you are emperor or slave and whether you are opponent. 
+  // do not have the cards hard coded on the HTML and use javascript to add
+
   const cards = document.querySelectorAll(".card");
   const opponent = document.querySelector(".opponent");
   const playButton = document.querySelector(".play");
