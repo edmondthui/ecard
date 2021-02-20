582b29ed75bfb113ccdbe07d1fd7c730e0667adf
Edmond Hui <edmond.t.hui@gmail.com>
2021-02-19 22:42:32 -0500

    :musical_note: add mute button for backround audio

--- client/src/app.js
+++ client/src/app.js
@@ -138,46 +138,6 @@ const setupBoard = (setup) => {
   }
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  // make a function that adds cards to each players hand based on whether you are emperor or slave and whether you are opponent.
-  // do not have the cards hard coded on the HTML and use javascript to add
-
-  const cards = document.querySelectorAll(".card");
-  const opponent = document.querySelector(".opponent");
-  const playButton = document.querySelector(".play");
-  let selected;
-
-  playButton.addEventListener("click", play);
-
-  cards.forEach((card) => {
-    if (!card.parentElement.classList.contains("opponent")) {
-      card.addEventListener("click", selectCard);
-    }
-  });
-
-  function selectCard(e) {
-    selected = e.currentTarget;
-    let selectedElements = document.querySelectorAll(".selected");
-    if (selectedElements.length < 1) {
-      e.currentTarget.classList.add("selected");
-    } else {
-      selectedElements[0].classList.remove("selected");
-      e.currentTarget.classList.add("selected");
-    }
-  }
-
-  function play(e) {
-    e.preventDefault();
-    cards.forEach((card) => {
-      if (card.classList.contains("selected")) {
-        card.classList.remove("selected");
-        let audio = new Audio("assets/zawazawa.wav");
-        audio.play();
-      }
-    });
-  }
-});
-
 (() => {
   const sock = io();
 
@@ -199,4 +159,58 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // document.querySelector(".create").addEventListener("click", hostGame(sock));
   document.querySelector(".join").addEventListener("click", joinGame(sock));
+
+  const audioButton = document.querySelector(".audio");
+  audioButton.addEventListener("click", () => {
+    const music = document.querySelector(".music");
+    if (music.paused) {
+      music.play();
+      audioButton.classList.remove("fa-volume-off");
+      audioButton.classList.add("fa-volume-up");
+    } else {
+      music.pause();
+      audioButton.classList.remove("fa-volume-up");
+      audioButton.classList.add("fa-volume-off");
+    }
+  });
 })();
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   // make a function that adds cards to each players hand based on whether you are emperor or slave and whether you are opponent.
+//   // do not have the cards hard coded on the HTML and use javascript to add
+
+//   const cards = document.querySelectorAll(".card");
+//   const opponent = document.querySelector(".opponent");
+//   const playButton = document.querySelector(".play");
+//   let selected;
+
+//   playButton.addEventListener("click", play);
+
+//   cards.forEach((card) => {
+//     if (!card.parentElement.classList.contains("opponent")) {
+//       card.addEventListener("click", selectCard);
+//     }
+//   });
+
+//   function selectCard(e) {
+//     selected = e.currentTarget;
+//     let selectedElements = document.querySelectorAll(".selected");
+//     if (selectedElements.length < 1) {
+//       e.currentTarget.classList.add("selected");
+//     } else {
+//       selectedElements[0].classList.remove("selected");
+//       e.currentTarget.classList.add("selected");
+//     }
+//   }
+
+//   function play(e) {
+//     e.preventDefault();
+//     cards.forEach((card) => {
+//       if (card.classList.contains("selected")) {
+//         card.classList.remove("selected");
+//         let audio = new Audio("assets/zawazawa.wav");
+//         audio.play();
+//       }
+//     });
+//   }
+// });
